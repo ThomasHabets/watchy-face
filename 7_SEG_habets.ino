8f27b0af3d924c3c65fb05b94f8d92bb69914122
Thomas Habets <thomas@habets.se>
2023-06-08 14:50:03 +0100

    Initial good version

---
+++ 7_SEG_habets.ino
@@ -0,0 +1,25 @@
+/*
+#include <BLE.h>
+#include <DSEG7_Classic_Bold_53.h>
+#include <Display.h>
+#include <Watchy.h>
+#include <WatchyRTC.h>
+#include <bma.h>
+#include <bma4.h>
+#include <bma423.h>
+#include <bma4_defs.h>
+#include <config.h>
+*/
+#include "Watchy_7_SEG.h"
+#include "settings.h"
+
+Watchy7SEG watchy(settings);
+
+void setup(){
+  watchy.init();
+}
+
+void loop(){}
+
+
+
